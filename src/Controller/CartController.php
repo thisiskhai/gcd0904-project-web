bcd055305ad0846bc0a81eca9f9f1874ea0cb266
phongttgcd <phongttgcd201885@fpt.edu.vn>
2022-03-09 13:23:28 +0700

    Add CRUD function and view

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -24,7 +24,6 @@ class CartController extends AbstractController
             'cart' => 'CartController',
         ]);
     }
-
     #[Route('/cart/all/ascending', name: 'cart_ascending')]
     public function listAscendingCart(): Response
     {   
@@ -44,5 +43,91 @@ class CartController extends AbstractController
             'cart' => $result
         ]);
     }
+    #[Route('/cart/create', name: 'cart_create', methods: ['GET', 'POST'])]
+    public function createAction(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $cart = new Cart();
+        $form = $this->createForm(CartType::class, $cart);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->persist($cart);
+            $entityManager->flush();
+            return $this->redirectToRoute('cart_ascending',[], Response::HTTP_SEE_OTHER);
+        }
+        return $this->renderForm('cart/create.html.twig',[
+            'cart'=>$cart,
+            'form'=>$form]);
+    }
+    #[Route('/cart/detail/{id}', name: 'cart_detail')]
+    public function detailAction($id)
+    {
+        $cart = $this->getDoctrine()
+            ->getRepository('App:Cart')
+            ->find($id);
+        return $this->render('cart/detail.html.twig', [
+            'cart' => $cart
+        ]);
+    }
+    #[Route('/cart/delete/{id}', name: 'cart_delete')]
+    public function deleteAction($id)
+    {
+        $em = $this
+            ->getDoctrine()
+            ->getManager();
+        $cart = $em
+              ->getRepository('App:Cart')
+              ->find($id);
+        $em->remove($cart);
+        $em->flush();
 
+        $this->addFlash(
+            'error',
+            'Cart deleted'
+        );
+
+        return $this->redirectToRoute('cart_ascending');
+    }
+    #[Route('/cart/edit/{id}', name: 'cart_edit')]
+    public function editAction($id, Request $request)
+    {
+        $em = $this
+            ->getDoctrine()
+            ->getManager();
+        $cart = $em
+              ->getRepository('App:Cart')
+              ->find($id);
+
+        $form = $this->createForm(CartType::class, $cart);
+
+        if ($this->saveEditedData($form, $request, $cart)) {
+            $this->addFlash(
+                'notice',
+                'Cart Edited'
+            );
+            return $this->redirectToRoute('cart_ascending');
+        }
+
+        return $this->render('cart/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+    public function saveEditedData($form, $request, $cart)
+    {
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $cart->setQuantity($request->request->get('cart')['quantity']);
+
+            $em = $this
+                ->getDoctrine()
+                ->getManager();
+            $em->persist($cart);
+            $em->flush();
+
+            return true;
+        }
+
+        return false;
+    }
 }
--- src/Repository/CategoryRepository.php
+++ src/Repository/CategoryRepository.php
@@ -2,26 +2,25 @@
 
 namespace App\Repository;
 
-use App\Entity\Cart;
-use App\Form\CartType;
+use App\Entity\Category;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @method Cart|null find($id, $lockMode = null, $lockVersion = null)
- * @method Cart|null findOneBy(array $criteria, array $orderBy = null)
- * @method Cart[]    findAll()
- * @method Cart[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method Category|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Category|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Category[]    findAll()
+ * @method Category[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class CartRepository extends ServiceEntityRepository
+class CategoryRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, Cart::class);
+        parent::__construct($registry, Category::class);
     }
 
     // /**
-    //  * @return Cart[] Returns an array of Cart objects
+    //  * @return Category[] Returns an array of Category objects
     //  */
     /*
     public function findByExampleField($value)
@@ -48,7 +47,7 @@ class CartRepository extends ServiceEntityRepository
         ;
     }
     */
-    public function getCartAscending()
+    public function getCategoryAscending()
     {
         return $this
             ->createQueryBuilder('c')
